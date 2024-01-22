4facd7bd5c07a85c438b83f231440ac7aefc557f
networkSorcerer <gigattfyhig@gmail.com>
2024-01-22 16:31:31 +0900

    algorithm

--- JAVA_ALGORISM/src/ch02/ReverseArray.java
+++ JAVA_ALGORISM/src/ch02/ReverseArray.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 
 class ReverseArray {
 	static void swap(int []a, int idx1, int idx2) {
-		int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
+		//교환이랑 비슷함
+		int t = a[idx1]; 
+		a[idx1] = a[idx2];
+		a[idx2] = t;
 	}
 	static void reverse(int []a) {
-		for(int i=0; i<a.length / 2; i++)
-			swap(a, i, a.length -i -1);
+		for(int i=0; i<a.length / 2; i++) //배열의 시작 점 부터 중간까지
+			swap(a, i, a.length -i -1); //배열의 반대편 부터 중간 지점까지를 swap 메서드를 통해서 교환
 	}
 	
 	public static void main(String []args) {
