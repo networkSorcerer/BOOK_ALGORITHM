67a9e7e06d8fc1b534fcb4eb4c28a64a71a4c4ef
networkSorcerer <gigattfyhig@gmail.com>
2024-01-22 13:35:35 +0900

    algorithm

--- JAVA_ALGORISM/src/ch02/ReverseArray.java
+++ JAVA_ALGORISM/src/ch02/ReverseArray.java
@@ -4,28 +4,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class ReverseArray {
-	static void swap(int [] a, int idx1, int idx2) {
-		int t = a[idx1]; a[idx1] = a[idx2]; a[idx2]=t;
+	static void swap(int []a, int idx1, int idx2) {
+		int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
 	}
-	
 	static void reverse(int []a) {
-		for (int i = 0; i < a.length/2; i++) 
+		for(int i=0; i<a.length / 2; i++)
 			swap(a, i, a.length -i -1);
 	}
 	
 	public static void main(String []args) {
 		Scanner stdIn = new Scanner(System.in);
 		
-		System.out.println("요솟수 :");
-		int num =stdIn.nextInt();
+		System.out.println("요솟수 : ");
+		int num = stdIn.nextInt();
 		
-		int[] x = new int[num];
+		int [] x = new int[num];
 		
-		for(int i = 0; i < num; i++) {
+		for(int i = 0; i < num; i ++) {
 			System.out.println("x[" + i + "] :");
 			x[i] = stdIn.nextInt();
 		}
-		
 		reverse(x);
 		
 		System.out.println("요소를 역순으로 정렬했습니다");
