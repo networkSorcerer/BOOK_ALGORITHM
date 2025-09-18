e7f466d4bb00a83b969e5becf3b0fb5ab06d64a7
networkSocerer <gigattfyhig@gmail.com>
2025-09-18 21:30:54 +0900

    알고리즘

--- JAVA_ALGORISM/src/ch02/ReverseArrayEx2.java
+++ JAVA_ALGORISM/src/ch02/ReverseArrayEx2.java
@@ -1,5 +1,34 @@
 package ch02;
 
-public class ReverseArrayEx2 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+class ReverseArrayEx2 {
+	static void swap(int []a, int idx1, int idx2) {
+		int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
+	}
+	
+	static void reverse(int []a) {
+		System.out.println(Arrays.toString(a));
+		for(int i = 0; i < a.length /2; i++) {
+			System.out.println("a[" + i + "]와 a[" +(a.length - i -1) +"]을 교환합니다");
+			swap(a, i, a.length -i -1);
+			System.out.println(Arrays.toString(a));
+		}
+	}
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner (System.in);
+		
+		System.out.println("요솟수는 :");
+		int num = stdIn.nextInt();
+		
+		int[] x = new int[num];
+		for(int i = 0; i < num; i++) {
+			System.out.print("x[" + i + "] : ");
+			x[i] = stdIn.nextInt();		
+		
+		}
+		reverse(x);
+		System.out.println("역순 정렬을 마쳤습니다");
+	}
 }
